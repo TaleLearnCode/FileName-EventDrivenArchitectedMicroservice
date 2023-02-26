a4fa755326809d3ba59a12bc8e84f60ae6a539d6
Chad Green <chadgreen@chadgreen.com>
2023-02-26 13:21:03 -0500

    Core database built

---
+++ Demos/MoveIn/ResidentManagement/RM.Database/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,7 @@
+﻿-- Standard Population Scripts
+:r .\Populate\Core\Populate_RowStatus.sql
+:r .\Populate\Core\Populate_WorldRegion.sql
+:r .\Populate\Core\Populate_Country.sql
+:r .\Populate\Core\Populate_CountryDivision.sql
+:r .\Populate\Core\Populate_Language.sql
+:r .\Populate\Core\Populate_LanguageCulture.sql
---
+++ Demos/MoveIn/ResidentManagement/RM.Database/Tables/Resident.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE RM.Resident
+(
+  ResidentId  INT           NOT NULL IDENTITY(1,1),
+  FirstName   NVARCHAR(100) NOT NULL,
+  MiddleName  NVARCHAR(100)     NULL,
+  LastName    NVARCHAR(100) NOT NULL,
+  DateOfBirth DATE          NOT NULL,
+  IsActive    BIT           NOT NULL CONSTRAINT dfResident_IsActive DEFAULT(1)
+)
