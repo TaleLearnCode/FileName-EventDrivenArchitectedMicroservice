0585a67669d6056355aced6b1acda70914cd4acb
Chad Green <83720996+ChadwickGreen@users.noreply.github.com>
2023-03-21 14:19:29 -0400

    Starting to work on Lease Manager

--- Demos/MoveIn/TestConsole/Program.cs
+++ Demos/MoveIn/TestConsole/Program.cs
@@ -1,22 +1,25 @@
-﻿using SLS.CM.Services;
-
-//RoomServices roomServices = new("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MoveIn-PM;Integrated Security=True");
+﻿//using SLS.Common.Services.Responses;
+//using SLS.PM.Services;
+//using SLS.PM.Services.Responses;
+//RoomServices roomServices = new("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=MoveIn-PM;Integrated Security=True");
 //ListResponse<RoomResponse> availableRooms = await roomServices.GetAvailableRoomsForCommunityAsync(22, 0, 0);
 //Console.WriteLine(availableRooms.TotalRecords);
 
 
-CareServices careServices = new("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MoveIn-CM;Integrated Security=True");
+using SLS.CM.Services;
+
+CareServices careServices = new("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=MoveIn-CM;Integrated Security=True");
 await careServices.ResidentMoveIn(new()
 {
-	ResidentId = 9999,
-	CommunityId = 22,
-	RoomId = 1254,
-	CareTypeId = 3,
-	FirstName = "Laura",
-	MiddleName = "Jean",
-	LastName = "Green",
-	DateOfBirth = new(1946, 8, 15),
-	AncillaryCares = new() { 1, 11 }
+  ResidentId = 9999,
+  CommunityId = 22,
+  RoomId = 1254,
+  CareTypeId = 3,
+  FirstName = "Laura",
+  MiddleName = "Jean",
+  LastName = "Green",
+  DateOfBirth = new(1946, 8, 15),
+  AncillaryCares = new() { 1, 11 }
 });
 
 //await careServices.ResidentMoveIn(new()
